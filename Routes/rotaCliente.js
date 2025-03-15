3d960556f0e02f64afe7c29abe57cfc2e5787f61
Juliano Campos dos Santos <juliano.kampus@gmail.com>
2025-03-15 15:54:45 -0300

    concluido

---
+++ Routes/rotaCliente.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import ClienteCtrl from "../controller/clienteCtrl.js";
+
+const rotaCLiente = Router(); // mini aplicação http
+
+const cliCtrl = new ClienteCtrl()
+
+rotaCLiente.get('/', cliCtrl.consultar)
+rotaCLiente.post('/', cliCtrl.gravar)
+rotaCLiente.put('/', cliCtrl.alterar)
+rotaCLiente.patch('/', cliCtrl.alterar)
+rotaCLiente.delete('/', cliCtrl.excluir)
+
+export default rotaCLiente;
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-let aulas = 'aula 2'
+import express from "express"
+import rotaCLiente from "./Routes/rotaCliente.js"
+
+const host = '0.0.0.0' // qq interface do host
+const app = express()
+const porta = 4000
+
+app.use("/clientes", rotaCLiente)
+
+app.listen(porta, host, () => {
+    console.log(`servidor backend em execução : http://${host}:${porta}`)
+})
